a0ddbb94d011129d118571acacfe8af1261e7ed0
fabianq846 <fabianq846@gmail.com>
2023-07-18 20:01:22 +0000

    Edicion del post y index, botones de megusta

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ApplicationRecord
   attr_accessor :description
-
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :reactions, dependent: :destroy
-
+  #accepts_nested_atributes_for :comments, reject_if: :no_name_category
   Kinds = %w[like dislike].freeze
 end
